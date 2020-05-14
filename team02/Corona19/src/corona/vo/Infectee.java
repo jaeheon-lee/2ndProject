09caf5152994793da6d193562b69b3f7fd2247f1
강제영 <wpdud001@gmail.com>
2020-05-14 12:09:12 +0900

    DAO & VO ver3

--- team02/Corona19/src/corona/vo/Infectee.java
+++ team02/Corona19/src/corona/vo/Infectee.java
@@ -5,14 +5,105 @@ import java.util.ArrayList;
 public class Infectee extends Person {
 	private int num;
 	private String infectionRoute; // 감염경로 : 해외냐, 지역
-	private String in_hospital; // 입원 날짜
 	private int infecter; // 날 감염시킨 사람
-	private int condition; // 사망여부? 치료 중=0, 완치 = 1, 사망 = 2
+	private String in_hospital; // 입원 날짜
+	private String treatDate; // 완치 날짜
+	private String deathDate; // 사망 날짜
 	private ArrayList<Place> allTrace;  // 동선 껍데기
 	
 	
 	public Infectee() {
 		
 	}
+
+
+	public Infectee(int ssn, String name, String address1, String address2, int age, String gender, String test_condition,
+			String isolation, String country, int num, String infectionRoute, int infecter, String in_hospital, String treatDate,
+			String deathDate, ArrayList<Place> allTrace) {
+		super(ssn, name, address1, address2, age, gender, test_condition, isolation, country);
+		this.num = num;
+		this.infectionRoute = infectionRoute;
+		this.infecter = infecter;
+		this.in_hospital = in_hospital;
+		this.treatDate = treatDate;
+		this.deathDate = deathDate;
+		this.allTrace = allTrace;
+	}
 	
+	public int getNum() {
+		return num;
+	}
+
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+
+	public String getInfectionRoute() {
+		return infectionRoute;
+	}
+
+
+	public void setInfectionRoute(String infectionRoute) {
+		this.infectionRoute = infectionRoute;
+	}
+
+
+	public int getInfecter() {
+		return infecter;
+	}
+
+
+	public void setInfecter(int infecter) {
+		this.infecter = infecter;
+	}
+
+
+	public String getIn_hospital() {
+		return in_hospital;
+	}
+
+
+	public void setIn_hospital(String in_hospital) {
+		this.in_hospital = in_hospital;
+	}
+
+
+	public String getTreatDate() {
+		return treatDate;
+	}
+
+
+	public void setTreatDate(String treatDate) {
+		this.treatDate = treatDate;
+	}
+
+
+	public String getDeathDate() {
+		return deathDate;
+	}
+
+
+	public void setDeathDate(String deathDate) {
+		this.deathDate = deathDate;
+	}
+
+
+	public ArrayList<Place> getAllTrace() {
+		return allTrace;
+	}
+
+
+	public void setAllTrace(ArrayList<Place> allTrace) {
+		this.allTrace = allTrace;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Infectee [num=" + num + ", infectionRoute=" + infectionRoute + ", infecter=" + infecter
+				+ ", in_hospital=" + in_hospital + ", treatDate=" + treatDate + ", deathDate=" + deathDate
+				+ ", allTrace=" + allTrace + "]";
+	}
 }
--- team02/Corona19/src/corona/vo/Person.java
+++ team02/Corona19/src/corona/vo/Person.java
@@ -17,6 +17,100 @@ public class Person {
 	public Person() {
 		
 	}
+
+	public Person(int ssn, String name, String address1, String address2, int age, String gender, String test_condition,
+			String isolation, String country) {
+		super();
+		this.ssn = ssn;
+		this.name = name;
+		this.address1 = address1;
+		this.address2 = address2;
+		this.age = age;
+		this.gender = gender;
+		this.test_condition = test_condition;
+		this.isolation = isolation;
+		this.country = country;
+	}
+
+	public int getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(int ssn) {
+		this.ssn = ssn;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getTest_condition() {
+		return test_condition;
+	}
+
+	public void setTest_condition(String test_condition) {
+		this.test_condition = test_condition;
+	}
+
+	public String getIsolation() {
+		return isolation;
+	}
+
+	public void setIsolation(String isolation) {
+		this.isolation = isolation;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [ssn=" + ssn + ", name=" + name + ", address1=" + address1 + ", address2=" + address2 + ", age="
+				+ age + ", gender=" + gender + ", test_condition=" + test_condition + ", isolation=" + isolation
+				+ ", country=" + country + "]";
+	}
+	
 	
 
 	
