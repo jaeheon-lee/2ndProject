1fe7b619b34408018a55a441a79c1f9c434f0107
LEEJAEHEON <ggapdol2@gmail.com>
2020-05-14 16:03:57 +0900

    testFile

---
+++ team02/Corona19/src/corona/test/Corona_test.java
@@ -0,0 +1,12 @@
+package corona.test;
+
+import corona.dao.Database;
+
+public class Corona_test {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		Database db = new Database("127.0.0.1");
+		
+	}
+
+}
